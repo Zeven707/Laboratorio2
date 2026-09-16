85ae185457add1d708ae5beab3fc3f513e2470be
Camila Aide Torrez Quispe <torrezquispecamilaaide@gmail.com>
2026-09-15 21:36:34 -0400

    Update mipunto.py

--- lab2/mipunto.py
+++ lab2/mipunto.py
@@ -2,46 +2,28 @@ import math
 
 
 class MiPunto:
-  def _init_(self, x=0.0, y=0.0):
-    self._x = float(x)
-    self._y = float(y)
+
+  def __init__(self, x=0, y=0):
+    self.x = x
+    self.y = y
 
   def get_x(self):
-    return self._x
+    return self.x
 
   def get_y(self):
-    return self._y
+    return self.y
 
- 
-  def distancia(self, *args):
-    if len(args) == 1 and isinstance(args[0], MiPunto):
-  
-      otro = args[0]
-      return math.sqrt(
-          (self._x - otro.get_x()) * 2 + (self._y - otro.get_y()) * 2
-      )
-    elif len(args) == 2:
+  def distancia(self, otro_punto):
+    return math.sqrt((self.x - otro_punto.x) ** 2 + (self.y - otro_punto.y) ** 2)
 
-      px, py = args[0], args[1]
-      return math.sqrt((self._x - px) * 2 + (self._y - py) * 2)
-    else:
-      raise ValueError("Parámetros no válidos para el método distancia.")
+  def __str__(self):
+    return f"{self.x}, {self.y}"
 
 
+if __name__ == "__main__":
+  punto1 = MiPunto(2, 3)
+  punto2 = MiPunto(7, 11)
 
-if _name_ == "_main_":
-
-  punto1 = MiPunto()
-
-
-  punto2 = MiPunto(10, 30.5)
-
-  distancia_resultado = punto1.distancia(punto2)
-
-  print(
-      f"Coordenadas del Punto 1: ({punto1.get_x()}, {punto1.get_y()})"
-  )
-  print(
-      f"Coordenadas del Punto 2: ({punto2.get_x()}, {punto2.get_y()})"
-  )
-  print(f"La distancia entre los dos puntos es: {distancia_resultado}")
+  print(f"Punto 1: {punto1}")
+  print(f"Punto 2: {punto2}")
+  print("La distancia es:", punto1.distancia(punto2))
